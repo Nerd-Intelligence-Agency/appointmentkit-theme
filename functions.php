6bf6cdfcd04a98d6cfdcfdbc0f0f25f2537a32fb
Bryant <bryant.cole@nerdintel.com>
2019-05-19 15:12:14 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
 if (!defined('ABSPATH')) die();
 
 //enque divi theme
-function ideakit_ct_enqueue_parent() {
+function appointmentkit_ct_enqueue_parent() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'appointmentkit_ct_enqueue_parent' );
@@ -15,7 +15,7 @@ add_action( 'wp_enqueue_scripts', 'appointmentkit_ct_enqueue_parent' );
 //Check for theme updates
 require 'plugin-update-checker/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-  'https://github.com/Nerd-Intelligence-Agency/AppointmentKit-Theme',
+  'https://github.com/Nerd-Intelligence-Agency/appointmentkit-theme',
   __FILE__,
   'appointmentkit-theme'
 );
